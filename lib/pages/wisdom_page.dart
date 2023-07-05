22b69dbb31cb17b682821d14596ff09a9253e5b8
Leonard Papilaya <leopapilaya@gmail.com>
2023-07-06 00:08:54 +0800

    Cleaning

--- lib/pages/wisdom_page.dart
+++ lib/pages/wisdom_page.dart
@@ -4,7 +4,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:wisdomday/blocs/quote_bloc.dart';
 import 'package:wisdomday/blocs/quote_event.dart';
 import 'package:wisdomday/blocs/quote_state.dart';
-import 'package:wisdomday/models/quote_model.dart';
 
 class WisdomPage extends StatefulWidget {
   const WisdomPage({Key? key}) : super(key: key);
@@ -14,23 +13,6 @@ class WisdomPage extends StatefulWidget {
 }
 
 class _WisdomPageState extends State<WisdomPage> {
-  // String _quoteText = 'waiting for a quote...';
-  // String _quoteAuthor = '';
-
-  QuoteBloc _quoteBloc = QuoteBloc();
-
-  // Future<void> _getQuote() async {
-  //   final dio = Dio();
-  //   final response = await dio.get('https://api.quotable.io/random');
-  //   // print(response.data);
-  //   QuoteModel model = QuoteModel.fromJson(response.data);
-  //   // print(model.content);
-  //
-  //   setState(() {
-  //     _quoteText = model.content.toString();
-  //     _quoteAuthor = model.author.toString();
-  //   });
-  // }
 
   @override
   Widget build(BuildContext context) {
